021640b9aec80fc8c7ae438477c00cf988a628cc
MADEYE <52187012+seyi91@users.noreply.github.com>
2021-06-02 21:13:17 -0500

    Add search() method for Search Bar, sets state of SearchBar to user input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -64,6 +64,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
 
   }
 
@@ -92,14 +93,20 @@ class App extends React.Component {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
 
+  search(query){
+    console.log(query);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} isRemoval={false} />
+            <SearchResults searchResults={this.state.searchResults} 
+                            onAdd={this.addTrack} 
+                            isRemoval={false} />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks} 
                       onRemove={this.removeTrack}
