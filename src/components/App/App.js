9c7a87f653074e1c0e96696fb27d2bbbdeece297
MADEYE <52187012+seyi91@users.noreply.github.com>
2021-06-02 20:55:22 -0500

    Add savePlaylist() method to save playlist to Spotify account

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -63,6 +63,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
 
   }
 
@@ -87,6 +88,10 @@ class App extends React.Component {
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -98,7 +103,8 @@ class App extends React.Component {
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks} 
                       onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName} />
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -21,7 +21,7 @@ class Playlist extends React.Component {
             <div className="Playlist">
                 <input defaultValue="New Playlist" onChange={this.handleNameChange}/>
                 <Tracklist tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
     }
